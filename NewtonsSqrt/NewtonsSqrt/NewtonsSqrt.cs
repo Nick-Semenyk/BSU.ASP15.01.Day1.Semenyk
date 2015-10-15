773dff3057d6d49242c85de89cf09a0f94cc7ed3
Nick-Semenyk <nick.semenyk@gmail.com>
2015-10-15 05:09:50 +0200

    1

---
+++ NewtonsSqrt/NewtonsSqrt/NewtonsSqrt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewtonsSqrt
+{
+    public class NewtonsSqrt
+    {
+        //public static double EPS { get; set; } = 0.01; //C#6 ?
+        private static double _EPS = 0.01;
+        public static double EPS
+        {
+            get 
+            {
+                return _EPS;
+            }
+            set 
+            {
+                _EPS = value;
+            } 
+        }
+
+        public static double Calculate(double x, int n)
+        {
+            if (x < 0 || n <= 0)
+            {
+                return double.NaN;
+                //throw new ArgumentException();
+            }
+            double xCurr;
+            double xNext = 1;
+            double xCurrPow;
+            do
+            {
+                xCurr = xNext;
+                xCurrPow = 1;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    xCurrPow *= xCurr;
+                }
+                xNext = ((n - 1) * xCurr + x / xCurrPow) / n;
+            } while (Math.Abs(xCurr - xNext) > EPS);
+            return xNext;
+        }
+    }
+}
---
+++ TestForNewtonsMethod/TestForNewtonsMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewtonsSqrt;
+
+
+namespace TestForNewtonsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 42;
+            int n = 5;
+            Console.WriteLine(Math.Pow(a, 1.0/n));
+            NewtonsSqrt.NewtonsSqrt.EPS = 0.0001;
+            Console.WriteLine(NewtonsSqrt.NewtonsSqrt.Calculate(a,n));
+            Console.Read();
+        }
+    }
+}
